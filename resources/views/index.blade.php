53e07aac7bab60047252837f90b7d0a2e582545a
Dimas Bagas <deniasitubagas@gmail.com>
2024-05-07 08:05:24 +0700

    setup laravel

---
+++ resources/views/index.blade.php
@@ -0,0 +1,21 @@
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel</title>
+
+    {{-- meta --}}
+    <meta name="robots" content="index, follow">
+    <meta name="googlebot" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1">
+    <meta name="bingbot" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1">
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body>
+    <h1>Hello World</h1>
+</body>
+
+</html>
